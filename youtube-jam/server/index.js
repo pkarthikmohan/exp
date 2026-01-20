d1015f36de65fc56972b0c1754bb50be4231ef4d
P Karthik Mohan <pkarthikmohan24@gmail.com>
2026-01-20 20:46:13 +0000

    Fix Express 5 wildcard route syntax

--- youtube-jam/server/index.js
+++ youtube-jam/server/index.js
@@ -204,7 +204,7 @@ io.on('connection', (socket) => {
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
